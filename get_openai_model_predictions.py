aa165aae7e88bc627789d9f92f42b87bf435be11
naveenmalla <s4namall@uni-trier.de>
2024-10-29 12:47:17 +0100

    working code for predictions

--- get_openai_model_predictions.py
+++ get_openai_model_predictions.py
@@ -1,6 +1,18 @@
+import os
 from openai import OpenAI
-client = OpenAI()
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the API key from the environment variable
+api_key = os.getenv('OPENAI_API_KEY')
+
+if not api_key:
+    raise ValueError("API key not found. Please set the OPENAI_API_KEY environment variable.")
+
+# Initialize the OpenAI client with the key
+client = OpenAI(api_key=api_key)
 completion = client.chat.completions.create(
   model="ft:gpt-4o-mini-2024-07-18:personal:trier-20-5-prompt-excluded:ANNq48Mj",
   messages= [
@@ -8,4 +20,4 @@ completion = client.chat.completions.create(
     {"role": "user", "content": "80 118 79 123 88 127 89 129 88 132 91 135 93 136 103 115 103 134 108 140 112 145 108 150 96 137 100 130 100 130 76 144 76 130 76 129 77 128 77 128"}
   ]
 )
-print(completion.choices[0].message)
+print(completion.choices[0].message.content)
